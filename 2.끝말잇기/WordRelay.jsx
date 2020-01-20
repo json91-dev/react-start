36369525b8211133bbb09286c469cbab349620d5
jungwoole91 <jungwoolee91@gmail.com>
2020-01-20 19:12:29 +0900

    word-relay, webpack-dev-server

--- 2.끝말잇기/WordRelay.jsx
+++ 2.끝말잇기/WordRelay.jsx
@@ -3,12 +3,51 @@ const { Component } = React;
 
 class WordRelay extends Component {
   state = {
-    text: 'Hello, webpack'
+    word: '제로다', // 첫단어
+    value: '',
+    result: '',
   };
+
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    if (this.state.word[this.state.word -1] === this.state.value[0]) {
+      this.setState({
+        result: '딩동댕',
+        word: this.state.value,
+        value: ''
+      });
+      this.input.focus();
+    } else {
+      this.setState({
+        result: '떙',
+        value: '',
+      });
+      this.input.focus();
+    }
+  };
+
+  onRefInput = (c) => {
+    this.input = c;
+  };
+
+  onChange = (e) => {
+    this.setState({value: e.target.value})
+  };
+
+  input;
 
   render() {
     return (
-      <h1>{this.state.text}</h1>
+      <>
+        <div>{this.state.word}</div>
+        <div>
+          <form onSubmit={this.onSubmitForm}>
+            <input ref={this.onRefInput}  value={this.state.value} onChange={this.onChange} />
+            <button>입력!</button>
+          </form>
+        </div>
+        <div>{this.state.result}</div>
+      </>
     );
   }
 }
--- 2.끝말잇기/webpack.config.js
+++ 2.끝말잇기/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   name: 'wordrelay-setting',
@@ -17,15 +18,30 @@ module.exports = {
       test: /\.jsx?/, //js와 jsx파일에 룰을 적용하겠다.
       loader: 'babel-loader', // 최신 문법을 지원하도록 babel을 지원한다.
       options: {
-        presets: ['@babel/preset-env', '@babel/preset-react'],
-        plugins: ['@babel/plugin-proposal-class-properties']
-      }
+        presets: [
+          ['@babel/preset-env', {
+            targets: {
+              browsers: ['> 1% in KR'],
+            },
+            debug: true
+          }],
+          '@babel/preset-react',
+        ],
+        plugins: [
+          '@babel/plugin-proposal-class-properties',
+          'react-hot-loader/babel'
+        ],
+      },
     }]
-
   },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({debug: true})
+  ],
 
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'app.js'
+    filename: 'app.js',
+    publicPath: '/dist/',
   }
 }
+
